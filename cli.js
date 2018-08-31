30459dcb85e58d0204f6cc56574fb7ad70a6b0cd
vagrant <vagrant@homenet.localhost>
2018-08-31 17:04:14 -0400

    updates before working on sketch

--- cli.js
+++ cli.js
@@ -11,74 +11,78 @@ rl.prompt();
 
 rl.on('line', function(line) {
     if (line === "exit") rl.close();
-    var tkn = line.split(" ");
-    switch (tkn[0]){
-    	case 'boards':
-    		console.log("Registered Boards: ", net.getBoards());
-    		break;
-    	case 'device':
-    		switch(tkn[1]){
-    			case 'create':
-    				let d;
-    				if (d = net.createDevice(tkn[2])){
-    					console.log('created device: ', d);
-    				} else {
-    					console.error('error creating device');
-    				}
-    				break;
-    			case 'list':
-    				console.log('Device Listing: ', net.listDevices());
-    				break;
-    			default:
-    				let di = net.getDevice(tkn[1]);
+    if (line === "") {
+    	
+    } else {
+	    var tkn = line.split(" ");
+	    switch (tkn[0]){
+	    	case 'boards':
+	    		console.log("Registered Boards: ", net.getBoards());
+	    		break;
+	    	case 'device':
+	    		switch(tkn[1]){
+	    			case 'create':
+	    				let d;
+	    				if (d = net.createDevice(tkn[2])){
+	    					console.log('created device: ', d);
+	    				} else {
+	    					console.error('error creating device');
+	    				}
+	    				break;
+	    			case 'list':
+	    				console.log('Device Listing: ', net.listDevices());
+	    				break;
+	    			default:
+	    				let di = net.getDevice(tkn[1]);
+		    			switch(tkn[2]){
+		    				default:
+		    					console.error("Unknown Device Sub-Command");
+		    					break;
+		    			}
+	    				console.error('Unknown Device Command');
+	    				break;
+	    		}
+	    		break;
+	    	case 'board':
+	    		if (d = net.getBoard(tkn[1])){
 	    			switch(tkn[2]){
-	    				default:
-	    					console.error("Unknown Device Sub-Command");
-	    					break;
+	    			case 'pinmode':
+	    				d.setPinMode(parseInt(tkn[3],10), parseInt(tkn[4],10));
+	    				break;
+	    			case 'data':
+	    				d.getData((d)=>{
+	        				console.log('Saved Board Data: ', d);
+	    				});
+	    				break;
+	    			case 'name':
+	    				d.setName(tkn[3]);
+	    				break;
+	    			case 'reboot':
+	    				d.reboot();
+	    				break;
+	    			case 'pulse':
+	    				d.pulse(parseInt(tkn[3], 10));
+	    				break;
+	    			case 'set':
+	    				d.setPinState(parseInt(tkn[3], 10), parseInt(tkn[4],10));
+	    				break;
+	    			default:
+	    				console.log('Unknown board command', line);
 	    			}
-    				console.error('Unknown Device Command');
-    				break;
-    		}
-    		break;
-    	case 'board':
-    		if (d = net.getBoard(tkn[1])){
-    			switch(tkn[2]){
-    			case 'pinmode':
-    				d.setPinMode(parseInt(tkn[3],10), parseInt(tkn[4],10));
-    				break;
-    			case 'data':
-    				d.getData((d)=>{
-        				console.log('Saved Board Data: ', d);
-    				});
-    				break;
-    			case 'name':
-    				d.setName(tkn[3]);
-    				break;
-    			case 'reboot':
-    				d.reboot();
-    				break;
-    			case 'pulse':
-    				d.pulse(parseInt(tkn[3], 10));
-    				break;
-    			case 'set':
-    				d.setPinState(parseInt(tkn[3], 10), parseInt(tkn[4],10));
-    				break;
-    			default:
-    				console.log('Unknown board command', line);
-    			}
-    		}
-    		break;
-    	case 'ip':
-    		console.log("Local UDP IP: ", net.getIP());
-    		break;
-    	case 'port':
-    		console.log("Local UDP Port: ", net.getPort());
-    		break;
-    	case 'exit':
-    		rl.close();
-    		break;
-    	default:
-    		console.log('Unknown Command: ', line);
+	    		}
+	    		break;
+	    	case 'ip':
+	    		console.log("Local UDP IP: ", net.getIP());
+	    		break;
+	    	case 'port':
+	    		console.log("Local UDP Port: ", net.getPort());
+	    		break;
+	    	case 'exit':
+	    		rl.close();
+	    		break;
+	    	default:
+	    		console.log('Unknown Command: ', line);
+	    }
     }
     rl.prompt();
 }).on('close',function(){
